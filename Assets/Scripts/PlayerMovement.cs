13dfd86c8723b09f680aa4ee07e404600546bf6c
Davlittl7 <davontelittleton77@gmail.com>
2023-10-14 19:31:51 -0400

    Added tilemap and School modified sprites within tilemap and player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask platformLayer;
+
+    private float horizontal, speed = 8f, jumpPower = 16f;
+    private bool isFacingRight = true; 
 
     void Start()
     {
@@ -14,6 +22,36 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if(!isFacingRight && horizontal > 0f) 
+            Flip();
+        else if (isFacingRight && horizontal < 0f)
+            Flip();
         
+    }
+
+    public void Jump(InputAction.CallbackContext context) 
+    {
+        if (context.performed && isGrounded()) rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        if (context.canceled && rb.velocity.y > 0f) rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f); 
+    }
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, platformLayer);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+
     }
 }
